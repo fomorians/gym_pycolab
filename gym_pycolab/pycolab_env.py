cc7613119e18a303586465fb0f8bfd64fc07fd12
wenkesj <samwenke@gmail.com>
2019-02-21 18:59:29 -0500

    updates to observation options

--- gym_pycolab/pycolab_env.py
+++ gym_pycolab/pycolab_env.py
@@ -24,7 +24,8 @@ class PyColabEnv(gym.Env):
                  action_space=None,
                  delay=30,
                  resize_scale=8,
-                 colors=None):
+                 colors=None,
+                 observation_type='layers'):
         """Create an `PyColabEnv` adapter to a `pycolab` game as a `gym.Env`.
 
         You can access the `pycolab.Engine` instance with `env.current_game`.
@@ -52,11 +53,25 @@ class PyColabEnv(gym.Env):
 
         self._render_order = list(reversed(not_ordered + test_game.z_order))
         self._observation_order = sorted(layers)
+        self._observation_type = observation_type        
 
-        self._game_shape = list(observations.board.shape) + [len(layers)]
+        if self._observation_type == 'layers':
+            channels = [len(layers)]
+            channel_max = 1.
+        elif self._observation_type == 'labels':
+            channels = [1]
+            channel_max = 1.
+        elif self._observation_type == 'rgb':
+            assert self._colors is not None, ''
+            channels = [3]
+            channel_max = 255.
+        else:
+            raise NotImplementedError(self._observation_type)
+
+        self._game_shape = list(observations.board.shape) + channels
         self.observation_space = spaces.Box(
             low=np.zeros(self._game_shape, np.float32), 
-            high=np.ones(self._game_shape, np.float32), 
+            high=np.ones(self._game_shape, np.float32) * channel_max, 
             dtype=np.float32)
         self.action_space = action_space
 
@@ -74,20 +89,33 @@ class PyColabEnv(gym.Env):
 
     def get_states(self, observations):
         """Transform the pycolab `rendering.Observations` to a state."""
-        return np.stack([
-            np.asarray(observations.layers[layer_key], np.float32) 
-            for layer_key in self._observation_order], axis=-1)
+        # TODO(wenkesj) cache this control flow.
+        if self._observation_type == 'layered':
+            return np.stack([
+                np.asarray(observations.layers[layer_key], np.float32) 
+                for layer_key in self._observation_order], axis=-1)
+        elif self._observation_type == 'labels':
+            board = np.zeros(self._game_shape, np.int32)
+            board_mask = np.zeros(self._game_shape, np.int32)
+            for key in self._render_order:
+                board_layer_mask = np.array(observations.layers[key]) * self._observation_order.index(key)
+                board = np.where(np.logical_not(board_mask), board_layer_mask, board)
+                board_mask = np.logical_or(board_layer_mask, board_mask)
+            return board.astype(np.float32)
+        elif self._observation_type == 'rgb':
+            return self.paint_image(observations.layers)
 
-    def paint_board(self, observations):
-        board = np.zeros(list(observations.board.shape) + [3], np.uint32)
-        board_mask = np.zeros(list(observations.board.shape) + [3], np.bool)
+    def _paint_board(self, layers):
+        board_shape = self._last_observations.board.shape
+        board = np.zeros(list(board_shape) + [3], np.uint32)
+        board_mask = np.zeros(list(board_shape) + [3], np.bool)
 
         for key in self._render_order:
             color = self._colors.get(key, (0, 0, 0))
             color = np.reshape(color, [1, 1, -1]).astype(np.uint32)
 
             # Broadcast the layer to [H, W, C].
-            board_layer_mask = np.array(observations.layers[key])[..., None]
+            board_layer_mask = np.array(layers[key])[..., None]
             board_layer_mask = np.repeat(board_layer_mask, 3, axis=-1)
 
             # Update the board with the new layer.
@@ -96,6 +124,28 @@ class PyColabEnv(gym.Env):
             # Update the mask.
             board_mask = np.logical_or(board_layer_mask, board_mask)
         return board
+
+    def paint_image(self, layers, board=None):
+        """Paint the layers into the board and return an RGB array."""
+        if self._colors:
+            img = self._paint_board(layers)
+        else:
+            assert board is not None, '`board` must not be `None` if there are no colors.'
+            img = board
+        img = np.repeat(np.repeat(
+            img, self.resize_scale, axis=0), self.resize_scale, axis=1)
+        if len(img.shape) != 3:
+            img = np.repeat(img[..., None], 3, axis=-1)
+        img = img.astype(np.uint8)
+        return img
+
+    def layers_from_board(self, board):
+        """Convert the observation board into into layers."""
+        board_layers = np.rollaxis(board, axis=-1)
+        layers = {}
+        for idx, key in enumerate(self._observation_order):
+            layers[key] = board_layers[idx]
+        return layers
 
     def _update_for_game_step(self, observations, reward):
         """Update internal state with data from an environment interaction."""
@@ -147,19 +197,15 @@ class PyColabEnv(gym.Env):
         """Render the board to the gym viewer."""
         if self._last_observations:
             img = self._last_observations.board
-            if self._colors:
-                img = self.paint_board(self._last_observations)
-            img = np.repeat(np.repeat(
-                img, self.resize_scale, axis=0), self.resize_scale, axis=1)
-            if len(img.shape) != 3:
-                img = np.repeat(img[..., None], 3, axis=-1)
+            layers = self._last_observations.layers
+            img = self.paint_image(layers, board=img)
         else:
             img = self._empty_board
             img = np.repeat(np.repeat(
                 img, self.resize_scale, axis=0), self.resize_scale, axis=1)
             if len(img.shape) != 3:
                 img = np.repeat(img[..., None], 3, axis=-1)
-        img = img.astype(np.uint8)
+            img = img.astype(np.uint8)
 
         if mode == 'rgb_array':
             return img
